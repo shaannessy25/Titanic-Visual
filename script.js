c07a972a0d0540f5584897643ef61f2049a0b374
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-01 12:11:40 -0800

    adding sort buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import data from './titanic-data.js'
 
 const titanicEmbarked = document.querySelector('#titanic-embarked')
-const portColor = { S: 'tomato', C: 'cornflowerblue', Q: 'orange', undefined: 'limegreen' }
+const portColor = { S: 'tomato', C: 'cornflowerblue', Q: 'orange', undefined: 'limegreen', total: 'gray' }
 
 const embarkedCount = data.reduce((acc, p) => {
   if (acc[p.fields.embarked] === undefined){
@@ -12,6 +12,8 @@ const embarkedCount = data.reduce((acc, p) => {
   return acc
 }, {})
 
+embarkedCount.total = data.length
+
 const embarkedKeys = Object.keys(embarkedCount)
 
 
@@ -19,15 +21,19 @@ embarkedKeys.forEach((e) => {
   const el = document.createElement('div')
   titanicEmbarked.appendChild(el)
   el.style.width = '30px'
-  el.style.height = `${embarkedCount[e]}px`
+  const count = embarkedCount[e]
+  const percent = count / data.length * 100
+  el.style.height = `${percent}%`
   el.style.backgroundColor = portColor[e]
+  el.style.margin = '2px'
 })
 
 
-
-
-
-
+titanicEmbarked.style.display = 'flex'
+titanicEmbarked.style.alignItems = 'flex-end'
+titanicEmbarked.style.border = '1px solid'
+titanicEmbarked.style.width = '300px'
+titanicEmbarked.style.height = '300px'
 
 
 
@@ -63,15 +69,29 @@ const passengers = data.map(p => {
 //   }
 //   return 1
 // })
+function sortEmbarked() {
+  data.sort((a, b) => {
+    if (a.fields.embarked < b.fields.embarked){
+      return -1
+    } else if (a.fields.embarked > b.fields.embarked){
+      return 1
+    }
+    return 0
+  })
+}
 
-data.sort((a, b) => {
-  if (a.fields.embarked < b.fields.embarked){
-    return -1
-  } else if (a.fields.embarked > b.fields.embarked){
-    return 1
-  }
-  return 0
-})
+function sortAge() {
+  data.sort((a, b) => {
+    return a.fields.age - b.fields.age
+  })
+}
+
+function sortFare() {
+  data.sort((a, b) => {
+    return a.fields.fare - b.fields.fare
+  })
+}
+
 
 data.sort((a, b) => {
   if (a.fields.sex === 'female'){
@@ -85,14 +105,90 @@ passengers.forEach(p => {
   titanic.appendChild(p)
 })
 
+
+
 // Let's loop over each passenger and set some styles 
-passengers.forEach((p, i) => {
-  p.style.width = '15px'
-  p.style.height = '15px'
-  p.style.borderRadius = data[i].fields.sex === 'female' ? '50%' : '0%'
-  p.style.opacity = data[i].fields.survived === 'Yes' ? '1' : '0.5'
-  p.style.backgroundColor = portColor[data[i].fields.embarked]
+function renderPassengers() {
+  passengers.forEach((p, i) => {
+    p.classList.add('passenger')
+    p.dataset.id = i
+    p.style.width = '15px'
+    p.style.height = '15px'
+    p.style.borderRadius = data[i].fields.sex === 'female' ? '50%' : '0%'
+    p.style.opacity = data[i].fields.survived === 'Yes' ? '1' : '0.5'
+    p.style.backgroundColor = portColor[data[i].fields.embarked]
+  })
+}
+
+
+renderPassengers()
+
+
+
+
+const passengerDetail = document.querySelector('#passenger-details')
+
+
+document.body.addEventListener('mouseover', (e) => {
+  if(e.target.matches('.passenger')){
+    const id = e.target.dataset.id
+    const fields = data[id].fields
+    passengerDetail.style.position = 'absolute'
+    passengerDetail.style.display = 'block'
+    passengerDetail.style.left = `${e.pageX + 3}px`
+    passengerDetail.style.top = `${e.pageY + 3}px`
+    passengerDetail.style.backgroundColor = '#fff'
+    passengerDetail.style.border = '1px solid'
+    passengerDetail.style.padding = '.5em'
+    passengerDetail.innerHTML = `
+   <strong>${fields.name}</strong>
+   <ul>
+    <li>Age: ${fields.age}</li>
+    <li>Fare: ${fields.fare}</li>
+   </ul>
+   `
+  }
 })
+
+document.body.addEventListener('mouseout', (e) => {
+  if (e.target.matches('.passenger')){
+    passengerDetail.style.display = 'none'
+  }
+})
+
+
+
+
+
+
+document.body.addEventListener('click', (e) => {
+  if(e.target.matches('.sort-by-age')){
+    sortAge()
+    renderPassengers()
+  } else if (e.target.matches('.sort-by-fare')){
+    sortFare()
+    renderPassengers()
+  } else if (e.target.matches('.sort-by-embarked')){
+    sortEmbarked()
+    renderPassengers()
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Challenges - 
 
